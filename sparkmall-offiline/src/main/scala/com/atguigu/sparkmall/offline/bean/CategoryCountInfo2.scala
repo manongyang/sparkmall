c065594f38172653dfb0df9903ac239c06f96b2b
jack <jack@163.com>
2019-09-26 02:04:12 +0800

    test

---
+++ sparkmall-offiline/src/main/scala/com/atguigu/sparkmall/offline/bean/CategoryCountInfo2.scala
@@ -0,0 +1,8 @@
+package com.atguigu.sparkmall.offline.bean
+
+case class CategoryCountInfo(taskId: String,
+                             categoryId: String,
+                             clickCount: Long,
+                             orderCount: Long,
+                             payCount: Long,
+                             timestamp : String)
